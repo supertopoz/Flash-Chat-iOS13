2166c02c2a8904d1420f756b6ecb42c925102fcb
supertopoz <deepsleepsounds@gmail.com>
2020-11-30 06:49:09 +0900

    installed firebase

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     //   passwordTextfield.autocorrectionType = .no
+    }
+    
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                guard error != nil else {
+                    print(error?.localizedDescription)
+                    return
+                }
+                self.performSegue(withIdentifier: "RegisterToChat", sender: nil)
+                
+            }
+        } else {
+            return
+        }
+
     }
     
 }
